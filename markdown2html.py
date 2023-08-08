338c8ab3b51ed7592e860d114e296a7fce4e0c0e
root <root@LAPTOP-J7HTNKBB>
2023-08-08 14:23:59 +0200

    Test

--- markdown2html.py
+++ markdown2html.py
@@ -10,39 +10,30 @@ import sys
 import markdown
 
 
-def markdown_to_html(markdown_file, output_file):
-    """ Check if the input markdown file exists
-    """
+def markdown_to_html(markdown_file, output_file):   
     if not os.path.exists(markdown_file):
         sys.stderr.write(f"Missing {markdown_file}\n")
         s
         ys.exit(1)
-        
-    """ Read the contents of the markdown file"""
+
     with open(markdown_file, "r") as f:
         markdown_content = f.read()
 
-    """ Convert markdown content to HTML"""
     html_content = markdown.markdown(markdown_content)
 
-    """ Write HTML content to the output file"""
     with open(output_file, "w") as f:
         f.write(html_content)
 
 
 if __name__ == "__main__":
-    """ Check if the correct number of arguments is provided"""
+
     if len(sys.argv) < 3:
         sys.stderr.write(f"Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
 
-    """ Get input markdown file and
-    output HTML file from command line arguments"""
     markdown_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    """Convert markdown to HTML and save to the output file"""
     markdown_to_html(markdown_file, output_file)
 
-    """ Exit with a success status code"""
     sys.exit(0)
