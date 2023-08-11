df2ddd27e18dd6ef23344d252470d0e38f6c0c07
root <root@LAPTOP-J7HTNKBB>
2023-08-11 11:18:07 +0200

    ...

--- markdown2html.py
+++ markdown2html.py
@@ -24,7 +24,6 @@ class MarkdownToHTMLConverter:
         Check if the Markdown file exists
         """
         if not os.path.exists(self.markdown_file):
-            # Print an error message and exit if the file is missing
             sys.stderr.write(f"Missing {self.markdown_file}\n")
             sys.exit(1)
 
