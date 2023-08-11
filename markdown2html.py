9b067ede5e75a3d1e29d03091358fd7bc5566a16
root <root@LAPTOP-J7HTNKBB>
2023-08-11 11:15:04 +0200

    ...

--- markdown2html.py
+++ markdown2html.py
@@ -10,14 +10,40 @@ import markdown
 # Define a class for converting Markdown to HTML
 class MarkdownToHTMLConverter:
     def __init__(self, markdown_file, output_file):
-        # Initialize the MarkdownToHTMLConverter with input and output file paths
+        """
+        Initialize the MarkdownToHTMLConverter
+        with input and output file paths
+        """
         self.markdown_file = markdown_file
         self.output_file = output_file
 
     def convert(self):
-        # Convert the input Markdown file to HTML and write it to the output file
-        # Check if the Markdown file exists
+        """
+        Convert the input Markdown file to HTML
+        and write it to the output file
+        Check if the Markdown file exists
+        """
         if not os.path.exists(self.markdown_file):
             # Print an error message and exit if the file is missing
             sys.stderr.write(f"Missing {self.markdown_file}\n")
-            sys.exit
+            sys.exit(1)
+
+        with open(self.markdown_file, "r") as f:
+            markdown_content = f.read()
+
+        html_content = markdown.markdown(markdown_content)
+
+        with open(self.output_file, "w") as f:
+            f.write(html_content)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    converter = MarkdownToHTMLConverter(markdown_file, output_file)
+    converter.convert()
